992fa95382d9b429338042ecb8158b3ac0ea47b1
huzhiqiang <56748572+hzq-up@users.noreply.github.com>
2022-10-18 03:09:31 +0800

    update: 删除多余文件

--- src/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-const andyCoin = new BlockChain(4);
-const transaction1 = new Transaction("addrSender1", "addrReceiver1", 10);
-const transaction2 = new Transaction("addrSender2", "addrReceiver2", 20);
-andyCoin.addTransaction(transaction1);
-andyCoin.addTransaction(transaction2);
-
-console.log(andyCoin)
-andyCoin.mineTransactionPool('addr4');
-console.log(andyCoin)
